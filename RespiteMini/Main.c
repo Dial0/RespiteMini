77ad31d73eda8c25e4cc755dca028a3c125278d7
Ethan <ethansimpson@xtra.co.nz>
2023-09-02 19:34:38 +1200

    tilemaploading and astar pathing

--- RespiteMini/Main.c
+++ RespiteMini/Main.c
@@ -23,9 +23,27 @@
 *
 ********************************************************************************************/
 
+#define tBRIDGE_HOUSE   0b0001000000000000
+#define tTREES          0b0000100000000000
+#define tWATER          0b0000010000000000
+#define tCLIFFS_MOUNT   0b0000001000000000
+#define tTUNDRA         0b0000000100000000
+#define tSNOW           0b0000000010000000
+#define tDARK_GRASS     0b0000000001000000
+#define tLIGHT_GRASS    0b0000000000100000
+#define tSWAMP_WATER    0b0000000000010000
+#define tBARREN         0b0000000000001000
+#define tDESERT         0b0000000000000100
+#define tSWAMP          0b0000000000000010
+#define tBASE           0b0000000000000001
+
+
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+enum MoveDir { North, South, East, West };
 
 typedef struct RenderParams {
     int mapViewPortWidth;
@@ -42,6 +60,32 @@ typedef struct iVec2 {
     int y;
 } iVec2;
 
+typedef struct WagonEntity {
+    iVec2 wagonTilePos;
+    Vector2 wagonWorldPos;
+    iVec2 wagonTargetTilePos;
+    int aniFrames;
+    float moveSpeed;
+    enum MoveDir aniDir;
+} WagonEntity;
+
+typedef struct WagonAni {
+    Texture2D Tex;
+
+    //FrameRecs
+    Rectangle North;
+    Rectangle altNorth;
+
+    Rectangle East;
+    Rectangle altEast;
+
+    Rectangle West;
+    Rectangle altWest;
+
+    Rectangle South;
+    Rectangle altSouth;
+
+} WagonAni;
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -52,7 +96,7 @@ iVec2 mapTileXYtoScreenXY(int MapX, int MapY, RenderParams param) {
     int tileYOffset = param.mapViewPortHeight / param.tileSize;
     int scrollOffset = (param.smoothScrollY / param.scale);
 
-    return (struct iVec2) { MapX* param.tileSize, (MapY + tileYOffset)* param.tileSize + mapRenderOffset % param.tileSize + scrollOffset };
+    return (struct iVec2) { MapX* param.tileSize, (-MapY + tileYOffset)* param.tileSize + mapRenderOffset % param.tileSize + scrollOffset };
 }
 
 iVec2 mapWorldXYtoScreenXY(float MapX, float MapY, RenderParams param) {
@@ -60,10 +104,8 @@ iVec2 mapWorldXYtoScreenXY(float MapX, float MapY, RenderParams param) {
     int tileYOffset = param.mapViewPortHeight / param.tileSize;
     int scrollOffset = (param.smoothScrollY / param.scale);
 
-    return (struct iVec2) { MapX* (float)(param.tileSize), (MapY + tileYOffset)* (float)(param.tileSize) + mapRenderOffset % param.tileSize + scrollOffset };
+    return (struct iVec2) { MapX* (float)(param.tileSize), (-MapY + tileYOffset)* (float)(param.tileSize) + mapRenderOffset % param.tileSize + scrollOffset };
 }
-
-
 
 void drawDebugGrid(RenderParams param) {
     int mapRenderOffset = -(param.mapHeight - param.mapViewPortHeight);
@@ -88,18 +130,6 @@ void drawDebugGrid(RenderParams param) {
 void calculatePath(iVec2 start, iVec2 end) {
     iVec2 inter = { start.x, end.y };
 }
-
-enum MoveDir { North, South, East, West };
-
-typedef struct WagonEntity {
-    iVec2 wagonTilePos;
-    Vector2 wagonWorldPos;
-    iVec2 wagonTargetTilePos;
-    int aniFrames;
-    float moveSpeed;
-    enum MoveDir aniDir;
-} WagonEntity;
-
 
 void moveWagon(WagonEntity* wagon) {
 
@@ -142,25 +172,6 @@ void moveWagon(WagonEntity* wagon) {
     }
 
 }
-
-typedef struct WagonAni {
-    Texture2D Tex;
-
-    //FrameRecs
-    Rectangle North;
-    Rectangle altNorth;
-
-    Rectangle East;
-    Rectangle altEast;
-
-    Rectangle West;
-    Rectangle altWest;
-
-    Rectangle South;
-    Rectangle altSouth;
-
-} WagonAni;
-
 
 void renderWagon(WagonEntity wagonEnt, RenderParams renderParams, WagonAni Ani ) {
     iVec2 newWagPixel = mapWorldXYtoScreenXY(wagonEnt.wagonWorldPos.x, wagonEnt.wagonWorldPos.y, renderParams);
@@ -209,6 +220,328 @@ void renderWagon(WagonEntity wagonEnt, RenderParams renderParams, WagonAni Ani )
 
 }
 
+int calcTileMoveCost(unsigned int tileData) {
+
+    int moveCost = 0;
+    int terrains = 0;
+
+    if (tSWAMP & tileData) {
+        moveCost += 3;
+        terrains += 1;
+    }
+    if (tDESERT & tileData) {
+        moveCost += 4;
+        terrains += 1;
+    }
+    if (tBARREN & tileData) {
+        moveCost += 2;
+        terrains += 1;
+    }
+    if (tLIGHT_GRASS & tileData) {
+        moveCost += 1;
+        terrains += 1;
+    }
+    if (tDARK_GRASS & tileData) {
+        moveCost += 2;
+        terrains += 1;
+    }
+    if (tSNOW & tileData) {
+        moveCost += 3;
+        terrains += 1;
+    }
+    if (tTUNDRA & tileData) {
+        moveCost += 4;
+        terrains += 1;
+    }
+
+    moveCost /= terrains; //terrain movement cost is averaged over the terrains that occupy that tile
+
+    if (tSWAMP_WATER & tileData) {
+        moveCost += 2;
+    }
+    if (tCLIFFS_MOUNT & tileData) {
+        moveCost += 3;
+    }
+    if (tWATER & tileData) {
+        moveCost += 1;
+    }
+    if (tTREES & tileData) {
+        moveCost += 1;
+    }
+
+
+
+    if (tBRIDGE_HOUSE & tileData) {
+        moveCost = 1; //towns and bridges are always the lowest move cost
+    }
+
+    return moveCost;
+}
+
+void renderTileInfo(int x, int y, unsigned int tileData) {
+    int textYHeight = y;
+    int textHeight = 32;
+    int textSpacing = 4;
+
+    int tileMoveCost = calcTileMoveCost(tileData);
+
+    char str[80];
+
+    sprintf_s(str,80, "Move Cost: %i", tileMoveCost);
+
+    DrawText(str, x, textYHeight, textHeight, RED);
+    textYHeight += textHeight + textSpacing;
+
+    if (tSWAMP & tileData) {
+        DrawText("Swamp", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tDESERT & tileData) {
+        DrawText("Desert", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tBARREN & tileData) {
+        DrawText("Barrens", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tSWAMP_WATER & tileData) {
+        DrawText("Deep Swamp", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tLIGHT_GRASS & tileData) {
+        DrawText("Medow", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tDARK_GRASS & tileData) {
+        DrawText("Grass", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tSNOW & tileData) {
+        DrawText("Snow", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tTUNDRA & tileData) {
+        DrawText("Tundra", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tCLIFFS_MOUNT & tileData) {
+        DrawText("Mountain", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tWATER & tileData) {
+        DrawText("Water", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tTREES & tileData) {
+        DrawText("Trees", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+    if (tBRIDGE_HOUSE & tileData) {
+        DrawText("Town", x, textYHeight, textHeight, RED);
+        textYHeight += textHeight + textSpacing;
+    }
+}
+
+unsigned int getTileData(unsigned char x, unsigned char y, unsigned char* mapData) {
+    unsigned char width = mapData[0];
+    unsigned char height = mapData[1];
+    int tileIdx = y * width + x;
+    unsigned int dataPos = 2 + tileIdx * 4;
+    unsigned int tileData;
+    memcpy(&tileData, mapData + dataPos, 4);
+    return tileData;
+}
+
+iVec2 mapIdxToXY(int tileIdx, int mapSizeX) {
+    int x = tileIdx % mapSizeX;
+    int y = tileIdx / mapSizeX;
+    return (struct iVec2) { x, y };
+}
+
+int hDistNodes(int start, int end, int mapSizeX) {
+    iVec2 startPos = mapIdxToXY(start, mapSizeX);
+    iVec2 endPos = mapIdxToXY(end, mapSizeX);
+    return abs(endPos.x - startPos.x) + abs(endPos.y - startPos.y);
+}
+
+int getNeighbour(int tileIdx, int dir, int mapSizeX, int mapSizeY) {
+    int i = tileIdx;
+    if (dir == 0) {
+        //North
+        if (i + mapSizeX < (mapSizeX * mapSizeY)) {
+            return i + mapSizeX;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (dir == 1) {
+        //East
+        if ((i / mapSizeX) == ((i + 1) / mapSizeX)) {
+            return i + 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (dir == 2) {
+        //West
+        if ((i / mapSizeX) == ((i - 1) / mapSizeX)) {
+            return i - 1;
+        }
+        else {
+            return -1;
+        }
+
+    }
+
+    if (dir == 3) {
+        //South
+        if (i - mapSizeX > 0) {
+            return i - mapSizeX;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    return -2;
+}
+
+int findAsPath(iVec2 startTile, iVec2 endTile, unsigned char* mapData, int* path, int pathMaxSize) {
+
+    int mapSizeX = mapData[0];
+    int mapSizeY = mapData[1];
+
+    int start = startTile.y * mapSizeX + startTile.x;
+    int end = endTile.y * mapSizeX + endTile.x;
+
+    int mapDataSizeInt = mapSizeX * mapSizeY; 
+    int mapDataSizeByte = mapDataSizeInt * sizeof(int);
+    int totalDataSizeByte = mapDataSizeByte * 4;
+
+    int* aStarData = malloc(totalDataSizeByte);
+
+    int* openSet = aStarData;
+    int openSetSize = 0;
+
+    openSet[0] = start;
+    openSetSize += 1;
+
+    int* cameFromIdx = aStarData + mapDataSizeInt;
+    int* gScore = aStarData + (mapDataSizeInt * 2);
+    int* fScore = aStarData + (mapDataSizeInt * 3);
+
+    for (int i = mapDataSizeInt; i < (mapDataSizeInt * 4); i++) {
+        aStarData[i] = -1;
+    }
+
+    gScore[start] = 0;
+
+    while (openSetSize > 0) {
+
+        //Get the node with the lowest fScore
+        int curIdx = 0;
+        int fScoreLow = fScore[openSet[0]];
+
+        for (int i = 1; i < openSetSize; i++) {
+            if (fScore[openSet[i]] < fScoreLow) {
+                fScoreLow = fScore[openSet[i]];
+                curIdx = i;
+            }
+        }
+
+        if (openSet[curIdx] == end) {
+
+            int pathSize = 1;
+            int curIdx = end;
+            while (curIdx != start) {
+                curIdx = cameFromIdx[curIdx];
+                pathSize += 1;
+            }
+
+            curIdx = end;
+            int skip = pathSize - pathMaxSize;
+            if (skip < 0) {
+                skip = 0;
+            }
+            int writeIdx = pathSize-1;
+            while (1) {
+                if (skip) {
+                    skip -= 1;
+                }
+                else {
+                    path[writeIdx] = curIdx;
+                }
+
+                if (curIdx == start) {
+                    break;
+                }
+                curIdx = cameFromIdx[curIdx];
+                writeIdx -= 1;
+            }
+
+            free(aStarData);
+
+            if (pathSize > pathMaxSize) {
+                pathSize = pathMaxSize;
+            }
+
+            return pathSize; //Return the Path we found
+        }
+
+        //check the neightbours of the current node
+        int curNode = openSet[curIdx];
+
+        //swap and drop current node
+        openSet[curIdx] = openSet[openSetSize - 1];
+        openSetSize -= 1;
+
+        for (int i = 0; i < 4; i++) {
+            int neighIdx = getNeighbour(curNode, i, mapSizeX, mapSizeY);
+            if (neighIdx == -1) {
+                continue;
+            }
+
+            //int tentative_gScore = calcPathg(curNode.tileIdx,cameFromIdx,map) + map.tile[curNode.tileIdx].moveCost;
+            //int curNeigh_gScore = calcPathg(neighIdx,cameFromIdx,map);
+
+
+            unsigned int dataPos = 2 + curNode * 4;
+            unsigned int tileData;
+            memcpy(&tileData, mapData + dataPos, 4);
+
+            int tentative_gScore = gScore[curNode] + calcTileMoveCost(tileData);
+            int curNeigh_gScore = gScore[neighIdx];
+
+            if ((tentative_gScore < curNeigh_gScore) || curNeigh_gScore == -1) {
+                cameFromIdx[neighIdx] = curNode;
+                gScore[neighIdx] = tentative_gScore;
+                fScore[neighIdx] = tentative_gScore + hDistNodes(neighIdx, end, mapSizeX);
+
+                // if not in openset add
+                int add = 1;
+                for (int i = 0; i < openSetSize; i++) {
+                    if (openSet[i] == neighIdx) {
+                        add = 0;
+                    }
+                }
+
+                if (add) {
+                    openSet[openSetSize] = neighIdx;
+                    openSetSize += 1;
+                }
+            }
+
+        }
+
+    }
+
+    free(aStarData);
+    return 0;
+}
 
 int main(void)
 {
@@ -269,6 +602,10 @@ int main(void)
 
     WagonEntity WagonEnt = { (struct iVec2) { 15,0 },(struct Vector2) { 15.0f,0.0f },(struct iVec2) { 15,0 },0, 0.01f };
 
+    unsigned int mapDataSize;
+    unsigned char* mapData = LoadFileData("respitetest.rspb", &mapDataSize);
+
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -278,10 +615,10 @@ int main(void)
         //----------------------------------------------------------------------------------
         if (IsKeyReleased(KEY_RIGHT)) cursTilePos.x += 1;
         if (IsKeyReleased(KEY_LEFT)) cursTilePos.x -= 1;
-        if (IsKeyReleased(KEY_UP)) cursTilePos.y -= 1;
-        if (IsKeyReleased(KEY_DOWN)) cursTilePos.y += 1;
+        if (IsKeyReleased(KEY_UP)) cursTilePos.y += 1;
+        if (IsKeyReleased(KEY_DOWN)) cursTilePos.y -= 1;
 
-        int ScrollLockHigh = -WagonEnt.wagonTilePos.y * tileSize * scale;
+        int ScrollLockHigh = WagonEnt.wagonTilePos.y * tileSize * scale;
         int ScrollLockLow = ScrollLockHigh - (baseSizeY - tileSize - 1) * scale;
 
         if (IsKeyDown(KEY_W) && smoothScrollY <= (map.height - baseSizeY + 1)*scale && smoothScrollY< ScrollLockHigh) smoothScrollY += 1;
@@ -305,6 +642,17 @@ int main(void)
         }
          
         moveWagon(&WagonEnt);
+
+
+        unsigned int tileData = getTileData(cursTilePos.x, cursTilePos.y, mapData);
+
+        int path[12];
+        int pathsize = 0;
+        if (movingWagon) {
+            pathsize = findAsPath(WagonEnt.wagonTilePos, cursTilePos, mapData, path, 12);
+        }
+        
+        
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -344,15 +692,32 @@ int main(void)
             DrawLineEx((struct Vector2) { wagPixel.x + tileSize / 2, wagPixel.y + tileSize / 2 }, (struct Vector2) { cursPixel.x + tileSize / 2, wagPixel.y + tileSize / 2 }, 4, PINK);
         }
 
-
+        if (pathsize) {
+            for (int i = 0; i < pathsize; i++) {
+                iVec2 pathPos = mapTileXYtoScreenXY(mapIdxToXY(path[i], 30).x, mapIdxToXY(path[i], 30).y, renderParams);
+                DrawRectangle(pathPos.x, pathPos.y, tileSize, tileSize, ColorAlpha(PINK, 0.5f));
+            }
+        }
         EndTextureMode();
 
 
         BeginTextureMode(gameRendRexTemp);
         DrawTexture(gameRendRex.texture, 0,0, WHITE);
+
         EndTextureMode();
 
+
+        
         DrawTextureEx(gameRendRexTemp.texture, (struct Vector2) { 0, -scale+smoothScrollY%scale }, 0.0f, scale, WHITE);
+
+        
+        renderTileInfo(map.width*scale + 10, 10, tileData);
+
+
+        DrawText("PATH", 10, 10, 32, RED);
+        
+        
+
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
